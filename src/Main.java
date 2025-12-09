5186256ff3bea4269b7a59dd512fbf3e8bf8a51c
Winzyss-code <134614274+Winzyss-code@users.noreply.github.com>
2025-12-09 12:33:46 +0500

    Added my assignment files for week 1

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Task 1
+       /* String Name   = "Abay";
+        int Age = 19;
+        String City = "Astana";*/
+
+
+        //Task 2
+        //int a=3,b=4;
+
+        //Task 3
+        /*int current_year = 3126;
+        int my_friend = 8;
+        int birth_year = current_year-my_friend;
+        System.out.println("Year my friend were born in " + birth_year);*/
+
+        //Task 4
+        /*String one = "dssdaasd";
+        String two = "Abababababa";
+        String three = "asdjkhkjhkjhkj";
+        String four = "askjdhkjhd";*/
+
+
+
+
+}
+}
